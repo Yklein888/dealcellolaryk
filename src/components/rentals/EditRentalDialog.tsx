e7570d02c7f1033648097825a6e7346af107ffbc
Yitzi <yklein89@gmail.com>
2026-03-05 16:50:23 +0200

    [sync] 5.3.2026, 16:50:23

--- src/components/rentals/EditRentalDialog.tsx
+++ src/components/rentals/EditRentalDialog.tsx
@@ -28,6 +28,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Checkbox } from '@/components/ui/checkbox';
+import { FormFieldHelper, FIELD_HINTS } from '@/components/FormFieldHelper';
 import { DualCurrencyPrice } from '@/components/DualCurrencyPrice';
 import { 
   X, 
